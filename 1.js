55f4f34e11000e6c955ea920d57454b67eef4857
shaonianzhentan <ilovevar@gmail.com>
2019-07-07 15:26:03 +0800

    update

---
+++ 1.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+
+let today = new Date()
+let config = {
+  ver: `${today.getFullYear()}.${today.getMonth() + 1}.${today.getDate()}.${today.getHours()}.${today.getMinutes()}`
+}
+fs.writeFileSync('./dist/config.json', JSON.stringify(config))
+
+let content = fs.readFileSync('./template.js', 'utf-8')
+
+fs.writeFileSync('./dist/baidu-map.js',
+  content.replace('custom-localhost', `/community_plugin/lovelace-cloud-music/index.html?ver=${config.ver}`))
+
---
+++ dist/precache-manifest.f5da2747ad03a6935dfa754b38fc9fc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "587db47d369e7e89c094",
+    "url": "/js/chunk-vendors.6f22d022.js"
+  },
+  {
+    "revision": "5438f92a696283cc8850",
+    "url": "/js/app.c543de80.js"
+  },
+  {
+    "revision": "94e466322ad848cd30e8cd387cda13de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "587db47d369e7e89c094",
+    "url": "/css/chunk-vendors.6a85daba.css"
+  },
+  {
+    "revision": "5438f92a696283cc8850",
+    "url": "/css/app.7ba26c2a.css"
+  }
+];
