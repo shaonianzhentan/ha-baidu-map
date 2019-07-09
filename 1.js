cd27560c80e9e34559ab2e12195081ae52b652ae
zhulu <shaonianzhentan@sina.cn>
2019-07-09 20:16:12 +0800

    测试

--- 1.js
+++ 1.js
@@ -6,16 +6,16 @@ let config = {
 }
 fs.writeFileSync('./dist/config.json', JSON.stringify(config))
 
-let content = fs.readFileSync('./dist/index.html', 'utf-8')
+// let content = fs.readFileSync('./dist/index.html', 'utf-8')
 
 
-let reg = /\/dev\//g
+// let reg = /\/dev\//g
 
 
-let baidu_dev = content.replace(reg,'https://raw.githubusercontent.com/shaonianzhentan/lovelace-baidu-map/master/dist/')
+// let baidu_dev = content.replace(reg,'https://raw.githubusercontent.com/shaonianzhentan/lovelace-baidu-map/master/dist/')
 
-fs.writeFileSync('./dist/baidu-dev.html', baidu_dev.replace(/\.\/js\//g,'https://raw.githubusercontent.com/shaonianzhentan/lovelace-baidu-map/master/dist/js/'))
+// fs.writeFileSync('./dist/baidu-dev.html', baidu_dev.replace(/\.\/js\//g,'https://raw.githubusercontent.com/shaonianzhentan/lovelace-baidu-map/master/dist/js/'))
 
-fs.writeFileSync('./dist/index.html', content.replace(reg,''))
+// fs.writeFileSync('./dist/index.html', content.replace(reg,''))
 
 fs.copyFileSync('./template.js','./dist/baidu-map.js')
